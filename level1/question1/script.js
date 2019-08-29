29dc7dc097b56bf1d3fe4e5c74c5edf5eee3231b
Marie Røgler <marie-r@hotmail.no>
2019-08-29 17:52:32 +0200

    answers

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,26 @@
 //Use RegEx to validate form
+
+function validateEmail(value) {
+    var emailexpression = /^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var phoneNumberExpresion = /^[0-9]{8}$/;
+}
+
+function submitFormButtonPressed() {
+    var email = "" // Get this from the form input somehow...
+    var emailExpression = /^[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    var isEmailValid = emailExpression.test(email);
+
+    if (isEmailValid === false) {
+        // Not valid email - tell the user.. abort abort!!
+        return
+    }
+
+    var firstName = "";
+
+    var isFirstnameValid = firstName.trim().length > 0;
+
+
+
+
+}
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,33 @@
 //Console.log out elements in JSON file
+
+var data = `
+{
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Aleijandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+            }]
+		}
+	}]
+}`
+
+data = JSON.parse(data);
+
+console.log(data);
+
+for (var i = 0; i < data.video.length; i++) {
+    var video = data.video[i];
+    console.log(video.id);
+    console.log(video.name);
+    console.log(video.url);
+
+    var authors = video.author.data;
+    for (var ii = 0; ii < authors.length; ii++) {
+        console.log(authors[ii].name_author);
+    }
+}
